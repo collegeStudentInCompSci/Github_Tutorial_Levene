9e98bc8e916edc6cf35bca8a26ddb8c79034b2e8
collegeStudentInCompSci <caleb.levene@ndus.edu>
2024-11-21 13:18:54 -0600

    MoviesDB and links

---
+++ Data/Github_TutorialContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorWebAppMovies.Models;
+
+namespace Github_Tutorial.Data
+{
+    public class Github_TutorialContext : DbContext
+    {
+        public Github_TutorialContext (DbContextOptions<Github_TutorialContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorWebAppMovies.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-using Github_Tutorial.Components;
+﻿using Github_Tutorial.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Github_Tutorial.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<Github_TutorialContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Github_TutorialContext") ?? throw new InvalidOperationException("Connection string 'Github_TutorialContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -14,6 +23,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
